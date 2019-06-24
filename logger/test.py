86aaad6b7a72c481175094db3bafa1b2ef0297b1
Nomura Hiroshi <h_nomura@echo.nuee.nagoya-u.ac.jp>
2019-06-24 18:59:09 +0900

    Update test.py

--- logger/test.py
+++ logger/test.py
@@ -1,23 +1,44 @@
-import time, datetime
-import csv
-import pprint
+# _*_ coding:utf-8 _*_
+# SIMPLE CURVE GRAPH PY
+# VERSION 1.0.0
+# ENV VERSION PYTHON 3.6.5,matplotlib 2.2.2
+# CREATED BY @pomta_trd
+
+# モジュールインポート
+import matplotlib.pyplot as plt
+
+# 初期化
+i = 0
+j = 0
+x = []
+y = []
+plt.ion()
+
+# MATPLOTLIB コンフィグ
+plt.title('Simple Curve Graph') ## グラフタイトル（必須ではない）
+plt.xlabel('x') ## x軸ラベル（必須ではない）
+plt.ylabel('y') ## y軸ラベル（必須ではない）
+plt.xlim(0,100) ## x軸範囲固定（必須ではない）
+plt.grid() ## グリッド線オン（必須ではない）
+
+# MAIN
 while True:
-    now = datetime.datetime.now()#get time
-    today = '{0:%Y-%m-%d}'.format(now)
-    with open('./data/MI{0:%y-%m-%d_%Hh%Mm%Ss}.csv'.format(now),'w') as f:
-        data = ['year_month_day','hour','minute','second','float','raw','Magnetic force(nT)']
-        writer = csv.writer(f)
-        writer.writerow(data)
-        counter = 0
-        while True:            
-            if counter == 1000:
-                print('  Magnetic force(nT)==')
-                counter = 0
-            
-            data = ['{0:%Y-%m-%d}'.format(now),'{0:%H}'.format(now),'{0:%M}'.format(now),'{0:%S}'.format(now),'{0:%f}'.format(now),value,convert_nT(value)]
-            writer = csv.writer(f)
-            writer.writerow(data)
-            counter += 1
-            if '{0:%Y-%m-%d}'.format(now) != today:
-                break
-            today = '{0:%Y-%m-%d}'.format(now)
+    ## 描画データ生成
+    i = i + 1
+    j = i ** 0.1
+    x.append(i) ### x軸データ
+    y.append(j) ### y軸データ
+    plt.plot(x,y,color='blue')
+
+    ## グラフ描画
+    plt.draw()
+
+    ## iが100ならWhileを抜ける
+    if i == 100:
+        break
+    
+    ## 更新待機（秒）
+    plt.pause(0.1)
+
+# グラフを閉じる
+plt.close()
